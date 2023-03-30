64ec49b144f509528344be160cd58c81d98577b4
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-03-30 04:40:56 -0400

    Fix op.gg if names have spaces

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,8 @@ use crate::lobby::Participant;
 pub fn create_opgg_link(summoners: Vec<Participant>) -> String {
     let mut opgg_link = "https://www.op.gg/multisearch/na?summoners=".to_string();
     for summoner in summoners {
-        opgg_link.push_str(&summoner.name);
+        let name_without_spaces = summoner.name.replace(' ', "%20");
+        opgg_link.push_str(&name_without_spaces);
         opgg_link.push(',');
     }
     opgg_link.pop();
